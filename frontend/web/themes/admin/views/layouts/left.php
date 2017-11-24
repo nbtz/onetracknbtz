7470a1c91c1c5de51bbe07bf5b8a2120b13bbdc8
nbtz <o_toey_namza_o@hotmail.com>
2017-11-25 02:34:56 +0700

    add me menu

--- frontend/web/themes/admin/views/layouts/left.php
+++ frontend/web/themes/admin/views/layouts/left.php
@@ -5,17 +5,17 @@
         <!-- Sidebar user panel -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
+                <img src="<?=$directoryAsset?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
             </div>
             <div class="pull-left info">
-                <p>Alexander Pierce</p>
+                <p>Admin</p>
 
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
 
         <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
+       <!--  <form action="#" method="get" class="sidebar-form">
             <div class="input-group">
                 <input type="text" name="q" class="form-control" placeholder="Search..."/>
               <span class="input-group-btn">
@@ -23,46 +23,85 @@
                 </button>
               </span>
             </div>
-        </form>
+        </form> -->
         <!-- /.search form -->
 
-        <?= dmstr\widgets\Menu::widget(
-            [
-                'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
-                'items' => [
-                    ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
-                    ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
-                    ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
-                    ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
-                    [
-                        'label' => 'Same tools',
-                        'icon' => 'share',
-                        'url' => '#',
-                        'items' => [
-                            ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii'],],
-                            ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug'],],
-                            [
-                                'label' => 'Level One',
-                                'icon' => 'circle-o',
-                                'url' => '#',
-                                'items' => [
-                                    ['label' => 'Level Two', 'icon' => 'circle-o', 'url' => '#',],
-                                    [
-                                        'label' => 'Level Two',
-                                        'icon' => 'circle-o',
-                                        'url' => '#',
-                                        'items' => [
-                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                        ],
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-            ]
-        ) ?>
+        <?=dmstr\widgets\Menu::widget(
+	[
+		'options' => ['class' => 'sidebar-menu tree', 'data-widget' => 'tree'],
+		'items' => [
+			['label' => 'Dashboard', 'options' => ['class' => 'header']],
+			[
+				'label' => 'ข้อมูลทีม',
+				'icon' => 'file-code-o',
+				'url' => ['#'],
+				'items' => [
+					['label' => 'จัดการข้อมูลทีม', 'icon' => 'file-code-o', 'url' => ['/#']],
+					['label' => 'วางแผนเข้าเยี่ยมลูกค้า', 'icon' => 'file-code-o', 'url' => ['/#']],
+					['label' => 'ข้อมูลเ็คอิน', 'icon' => 'file-code-o', 'url' => ['/#']],
+				],
+
+			],
+			[
+				'label' => 'ข้อมูลลูกค้า',
+				'icon' => 'file-code-o',
+				'url' => ['#'],
+				'items' => [
+					['label' => 'ลูกค้า', 'icon' => 'file-code-o', 'url' => ['/#']],
+					['label' => 'ประเภทลูกค้า', 'icon' => 'file-code-o', 'url' => ['/#']],
+					['label' => 'สถานะลูกค้า', 'icon' => 'file-code-o', 'url' => ['/#']],
+				],
+
+			],
+			[
+				'label' => 'ความปลอดภัย',
+				'icon' => 'file-code-o',
+				'url' => ['#'],
+				'items' => [
+					['label' => 'ผู้ใช้งาน', 'icon' => 'file-code-o', 'url' => ['/#']],
+				],
+
+			],
+
+			[
+				'label' => 'Logout',
+				'icon' => 'fa fa-sign-out',
+				'url' => ['/site/logout'],
+				'template' => '<a href="{url}" data-method="post">{icon} {label}</a>',
+				'visible' => !Yii::$app->user->isGuest,
+			],
+			// ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+
+			// ['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
+			/*[
+				'label' => 'Same tools',
+				'icon' => 'share',
+				'url' => '#',
+				'items' => [
+					['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii']],
+					['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
+					[
+						'label' => 'Level One',
+						'icon' => 'circle-o',
+						'url' => '#',
+						'items' => [
+							['label' => 'Level Two', 'icon' => 'circle-o', 'url' => '#'],
+							[
+								'label' => 'Level Two',
+								'icon' => 'circle-o',
+								'url' => '#',
+								'items' => [
+									['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#'],
+									['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#'],
+								],
+							],
+						],
+					],
+				],
+			],*/
+		],
+	]
+)?>
 
     </section>
 
