633fe225db8429eeca8a0e1d16139713718d57ef
nbtz <o_toey_namza_o@hotmail.com>
2017-11-24 18:48:36 +0700

    edit layout

--- frontend/web/themes/admin/views/layouts/main.php
+++ frontend/web/themes/admin/views/layouts/main.php
@@ -1,16 +1,34 @@
 <?php
-use Yii;
-use yii\widgets\Breadcrumbs;
 use yii\helpers\Html;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
 
 
+if (Yii::$app->controller->action->id === 'login') { 
+/**
+ * Do not use this code in your template. Remove it. 
+ * Instead, use the code  $this->layout = '//main-login'; in your controller.
+ */
+    echo $this->render(
+        'main-login',
+        ['content' => $content]
+    );
+} else {
+
+    if (class_exists('backend\assets\AppAsset')) {
+        backend\assets\AppAsset::register($this);
+    } else {
+        app\assets\AppAsset::register($this);
+    }
+
+    dmstr\web\AdminLteAsset::register($this);
+
+    $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
     ?>
-<?php $this->beginPage() ?>
-<!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
+    <?php $this->beginPage() ?>
+    <!DOCTYPE html>
+    <html lang="<?= Yii::$app->language ?>">
     <head>
         <meta charset="<?= Yii::$app->charset ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,37 +36,30 @@ use yii\helpers\Html;
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
     </head>
-    <body class="skin-blue sidebar-mini">
-        <div id="page-loader" class="fade in"><span class="spinner"></span></div>
-        <!-- end #page-loader -->
-    
-        <!-- begin #page-container -->
-        <div id="page-container" class="fade page-sidebar-fixed page-header-fixed">
-            <?php $this->beginBody() ?>
-                <div class="wrapper">
+    <body class="hold-transition skin-blue sidebar-mini">
+    <?php $this->beginBody() ?>
+    <div class="wrapper">
 
-                    <?= $this->render(
-                        'header.php'
-                    ) ?>
+        <?= $this->render(
+            'header.php',
+            ['directoryAsset' => $directoryAsset]
+        ) ?>
 
-                    <?= $this->render(
-                        'left.php'
-                    )
-                    ?>
+        <?= $this->render(
+            'left.php',
+            ['directoryAsset' => $directoryAsset]
+        )
+        ?>
 
-                    <?= $this->render(
-                        'content.php',
-                        ['content' => $content]
-                    ) ?>
+        <?= $this->render(
+            'content.php',
+            ['content' => $content, 'directoryAsset' => $directoryAsset]
+        ) ?>
 
-                    <!-- <section class="content">
-                        <?= $content ?>
-                    </section> -->
+    </div>
 
-                </div>
-
-            <?php $this->endBody() ?>
-        </div>
+    <?php $this->endBody() ?>
     </body>
-</html>
-<?php $this->endPage() ?>
+    </html>
+    <?php $this->endPage() ?>
+<?php } ?>
