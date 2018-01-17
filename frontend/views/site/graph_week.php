35bb17394f2cc660cb28a4b1e00b5a6210d44915
nbtz <o_toey_namza_o@hotmail.com>
2018-01-17 09:59:00 +0700

    edit bug company id graph page

--- frontend/views/site/graph_week.php
+++ frontend/views/site/graph_week.php
@@ -5,81 +5,11 @@ use miloschuman\highcharts\Highcharts;
 ?>
 
 <?php
-if (isset(Yii::$app->user->identity->company->id) && !empty(Yii::$app->user->identity->company->id)) {
-
-	$modelTeam = Bu::find()->where(['company_id' => Yii::$app->user->identity->company->id])->limit(8)->all();
-
-}
-$positionY = 80;
-$positionX = 100;
-$dataUser = [];
-$objects = [];
-
-for ($i = 0; $i < count($modelTeam); $i++) {
-	if ($i == 0) {
-
-	} else if ($i == 4) {
-		$positionX = 100;
-		$positionY = 280;
-	} else {
-		$positionX += 200;
-	}
-	// if (isset($modelTeam[$i]->users) && !empty($modelTeam[$i]->users)) {
-	// 	for ($j = 0; $j < count($modelTeam[$i]->users); $j++) {
-
-	// 		// $dataUser =
-
-	// 	}
-
-	// }
-
-	$objects[] = [
-		'type' => 'pie',
-		'name' => $modelTeam[$i]->bu_name,
-		'innerSize' => '70%',
-		'data' => [
-			[
-				'name' => 'Y',
-				'y' => 13,
-				'color' => '#68BE63', // Jane's color
-			],
-			[
-				'name' => 'N',
-				'y' => 23,
-				'color' => '#95999C', // John's color
-			],
-
-		],
-		'center' => [$positionX, $positionY],
-		'size' => 100,
-		'showInLegend' => false,
-		'dataLabels' => [
-			'enabled' => false,
-		],
-		// 'tooltip' => [
-		// 	'valueSuffix' => ' km/h',
-		// ],
-	];
-}
-
-?>
-
-
-
-
-
-
-<?php
 $num_today = date('w');
 $today = date("Y-m-d");
 // $today = date("Y-m-d", strtotime('2017-05-13'));
 $sunday = date("Y-m-d", strtotime($today . ' - ' . $num_today . ' days'));
 echo $sunday . " - " . $today . "<br><br>";
-
-$modelCheckin = CheckIn::find()->where(['company_id' => Yii::$app->user->identity->company->id])->andWhere(['between', 'DATE(chk_time)', $sunday, $today])->all();
-for ($i = 0; $i < count($modelCheckin); $i++) {
-	// echo $modelCheckin[$i]->chk_time . "<br>";
-}
 
 /*echo Highcharts::widget([
 'scripts' => [
@@ -114,60 +44,122 @@ for ($i = 0; $i < count($modelCheckin); $i++) {
 ?>
 <div class="row">
     <div class="col-md-8">
+    	<?php
+if (isset(Yii::$app->user->identity->company->id) && !empty(Yii::$app->user->identity->company->id)) {
+
+	$modelTeam = Bu::find()->where(['company_id' => Yii::$app->user->identity->company->id])->limit(8)->all();
+
+	$positionY = 80;
+	$positionX = 100;
+	$dataUser = [];
+	$objects = [];
+
+	for ($i = 0; $i < count($modelTeam); $i++) {
+		if ($i == 0) {
+
+		} else if ($i == 4) {
+			$positionX = 100;
+			$positionY = 280;
+		} else {
+			$positionX += 200;
+		}
+		// if (isset($modelTeam[$i]->users) && !empty($modelTeam[$i]->users)) {
+		// 	for ($j = 0; $j < count($modelTeam[$i]->users); $j++) {
+
+		// 		// $dataUser =
+
+		// 	}
+
+		// }
+
+		$objects[] = [
+			'type' => 'pie',
+			'name' => $modelTeam[$i]->bu_name,
+			'innerSize' => '70%',
+			'data' => [
+				[
+					'name' => 'Y',
+					'y' => 13,
+					'color' => '#68BE63', // Jane's color
+				],
+				[
+					'name' => 'N',
+					'y' => 23,
+					'color' => '#95999C', // John's color
+				],
+
+			],
+			'center' => [$positionX, $positionY],
+			'size' => 100,
+			'showInLegend' => false,
+			'dataLabels' => [
+				'enabled' => false,
+			],
+			// 'tooltip' => [
+			// 	'valueSuffix' => ' km/h',
+			// ],
+		];
+	}
+
+	$modelCheckin = CheckIn::find()->where(['company_id' => Yii::$app->user->identity->company->id])->andWhere(['between', 'DATE(chk_time)', $sunday, $today])->all();
+	for ($i = 0; $i < count($modelCheckin); $i++) {
+		// echo $modelCheckin[$i]->chk_time . "<br>";
+	}
+	?>
         <div class="row">
             <div class="col-xs-6 col-sm-3 box-graph" >
                 <div class="row"><div class="col-sm-6"> <h5>Team A</h5></div><div class="col-sm-6 right"><h5><i class="fa fa-ellipsis-v"></i></h5></div> </div>
                     <div style="overflow: hidden; height: 160px; width: 200;">
                 <?php
 echo Highcharts::widget([
-	'scripts' => [
-		'modules/exporting',
-		'themes/grid-light',
-	],
-	'options' => [
-		'title' => [
-			'text' => '36.11%',
-			'y' => 78,
+		'scripts' => [
+			'modules/exporting',
+			'themes/grid-light',
 		],
-		'series' => [
-			[
-				'type' => 'pie',
-				'name' => 'Team A',
-				'innerSize' => '70%',
-				'data' => [
-					[
-						'name' => 'Y',
-						'y' => 33,
-						'color' => '#68BE63', // Jane's color
+		'options' => [
+			'title' => [
+				'text' => '36.11%',
+				'y' => 78,
+			],
+			'series' => [
+				[
+					'type' => 'pie',
+					'name' => 'Team A',
+					'innerSize' => '70%',
+					'data' => [
+						[
+							'name' => 'Y',
+							'y' => 33,
+							'color' => '#68BE63', // Jane's color
+						],
+						[
+							'name' => 'N',
+							'y' => 13,
+							'color' => '#95999C', // John's color
+						],
+
 					],
-					[
-						'name' => 'N',
-						'y' => 13,
-						'color' => '#95999C', // John's color
+					'center' => [40, 20],
+					// 'size' => 100,
+					'showInLegend' => false,
+					'dataLabels' => [
+						'enabled' => false,
 					],
 
 				],
-				'center' => [40, 20],
-				// 'size' => 100,
-				'showInLegend' => false,
-				'dataLabels' => [
+			],
+			'navigation' => [
+				'buttonOptions' => [
 					'enabled' => false,
 				],
-
 			],
-		],
-		'navigation' => [
-			'buttonOptions' => [
+			'credits' => [
 				'enabled' => false,
 			],
 		],
-		'credits' => [
-			'enabled' => false,
-		],
-	],
-]);
+	]);
 
-?>
+	?>
                 </div>
             </div>
 
@@ -176,55 +168,55 @@ echo Highcharts::widget([
                     <div style="overflow: hidden; height: 160px; width: 200;">
                 <?php
 echo Highcharts::widget([
-	'scripts' => [
-		'modules/exporting',
-		'themes/grid-light',
-	],
-	'options' => [
-		'title' => [
-			'text' => '55.5%',
-			// 'align' => 'center',
-			// 'verticalAlign' => 'middle',
-			'y' => 78,
+		'scripts' => [
+			'modules/exporting',
+			'themes/grid-light',
 		],
-		'series' => [
-			[
-				'type' => 'pie',
-				'name' => 'Team B',
-				'innerSize' => '70%',
-				'data' => [
-					[
-						'name' => 'Y',
-						'y' => 13,
-						'color' => '#68BE63', // Jane's color
-					],
-					[
-						'name' => 'N',
-						'y' => 23,
-						'color' => '#95999C', // John's color
-					],
+		'options' => [
+			'title' => [
+				'text' => '55.5%',
+				// 'align' => 'center',
+				// 'verticalAlign' => 'middle',
+				'y' => 78,
+			],
+			'series' => [
+				[
+					'type' => 'pie',
+					'name' => 'Team B',
+					'innerSize' => '70%',
+					'data' => [
+						[
+							'name' => 'Y',
+							'y' => 13,
+							'color' => '#68BE63', // Jane's color
+						],
+						[
+							'name' => 'N',
+							'y' => 23,
+							'color' => '#95999C', // John's color
+						],
 
+					],
+					'center' => [40, 20],
+					// 'size' => 100,
+					'showInLegend' => false,
+					'dataLabels' => [
+						'enabled' => false,
+					],
 				],
-				'center' => [40, 20],
-				// 'size' => 100,
-				'showInLegend' => false,
-				'dataLabels' => [
+			],
+			'navigation' => [
+				'buttonOptions' => [
 					'enabled' => false,
 				],
 			],
-		],
-		'navigation' => [
-			'buttonOptions' => [
+			'credits' => [
 				'enabled' => false,
 			],
 		],
-		'credits' => [
-			'enabled' => false,
-		],
-	],
-]);
+	]);
 
-?>
+	?>
 				</div>
             </div>
 
@@ -233,55 +225,55 @@ echo Highcharts::widget([
                     <div style="overflow: hidden; height: 160px; width: 200;">
                 <?php
 echo Highcharts::widget([
-	'scripts' => [
-		'modules/exporting',
-		'themes/grid-light',
-	],
-	'options' => [
-		'title' => [
-			'text' => '8.99%',
-			// 'align' => 'center',
-			// 'verticalAlign' => 'middle',
-			'y' => 78,
+		'scripts' => [
+			'modules/exporting',
+			'themes/grid-light',
 		],
-		'series' => [
-			[
-				'type' => 'pie',
-				'name' => 'Team C',
-				'innerSize' => '70%',
-				'data' => [
-					[
-						'name' => 'Y',
-						'y' => 13,
-						'color' => '#68BE63', // Jane's color
-					],
-					[
-						'name' => 'N',
-						'y' => 23,
-						'color' => '#95999C', // John's color
-					],
+		'options' => [
+			'title' => [
+				'text' => '8.99%',
+				// 'align' => 'center',
+				// 'verticalAlign' => 'middle',
+				'y' => 78,
+			],
+			'series' => [
+				[
+					'type' => 'pie',
+					'name' => 'Team C',
+					'innerSize' => '70%',
+					'data' => [
+						[
+							'name' => 'Y',
+							'y' => 13,
+							'color' => '#68BE63', // Jane's color
+						],
+						[
+							'name' => 'N',
+							'y' => 23,
+							'color' => '#95999C', // John's color
+						],
 
+					],
+					'center' => [40, 20],
+					// 'size' => 100,
+					'showInLegend' => false,
+					'dataLabels' => [
+						'enabled' => false,
+					],
 				],
-				'center' => [40, 20],
-				// 'size' => 100,
-				'showInLegend' => false,
-				'dataLabels' => [
+			],
+			'navigation' => [
+				'buttonOptions' => [
 					'enabled' => false,
 				],
 			],
-		],
-		'navigation' => [
-			'buttonOptions' => [
+			'credits' => [
 				'enabled' => false,
 			],
 		],
-		'credits' => [
-			'enabled' => false,
-		],
-	],
-]);
+	]);
 
-?>
+	?>
 				</div>
             </div>
             <div class="col-xs-6 col-sm-3 box-graph">
@@ -289,55 +281,55 @@ echo Highcharts::widget([
                     <div style="overflow: hidden; height: 160px; width: 200;">
                 <?php
 echo Highcharts::widget([
-	'scripts' => [
-		'modules/exporting',
-		'themes/grid-light',
-	],
-	'options' => [
-		'title' => [
-			'text' => '33%',
-			// 'align' => 'center',
-			// 'verticalAlign' => 'middle',
-			'y' => 78,
+		'scripts' => [
+			'modules/exporting',
+			'themes/grid-light',
 		],
-		'series' => [
-			[
-				'type' => 'pie',
-				'name' => 'Team D',
-				'innerSize' => '70%',
-				'data' => [
-					[
-						'name' => 'Y',
-						'y' => 13,
-						'color' => '#68BE63', // Jane's color
-					],
-					[
-						'name' => 'N',
-						'y' => 23,
-						'color' => '#95999C', // John's color
-					],
+		'options' => [
+			'title' => [
+				'text' => '33%',
+				// 'align' => 'center',
+				// 'verticalAlign' => 'middle',
+				'y' => 78,
+			],
+			'series' => [
+				[
+					'type' => 'pie',
+					'name' => 'Team D',
+					'innerSize' => '70%',
+					'data' => [
+						[
+							'name' => 'Y',
+							'y' => 13,
+							'color' => '#68BE63', // Jane's color
+						],
+						[
+							'name' => 'N',
+							'y' => 23,
+							'color' => '#95999C', // John's color
+						],
 
+					],
+					'center' => [40, 20],
+					// 'size' => 100,
+					'showInLegend' => false,
+					'dataLabels' => [
+						'enabled' => false,
+					],
 				],
-				'center' => [40, 20],
-				// 'size' => 100,
-				'showInLegend' => false,
-				'dataLabels' => [
+			],
+			'navigation' => [
+				'buttonOptions' => [
 					'enabled' => false,
 				],
 			],
-		],
-		'navigation' => [
-			'buttonOptions' => [
+			'credits' => [
 				'enabled' => false,
 			],
 		],
-		'credits' => [
-			'enabled' => false,
-		],
-	],
-]);
+	]);
 
-?>
+	?>
 				</div>
             </div>
             <div class="col-xs-6 col-sm-3 box-graph">
@@ -345,55 +337,55 @@ echo Highcharts::widget([
                     <div style="overflow: hidden; height: 160px; width: 200;">
                 <?php
 echo Highcharts::widget([
-	'scripts' => [
-		'modules/exporting',
-		'themes/grid-light',
-	],
-	'options' => [
-		'title' => [
-			'text' => '30%',
-			// 'align' => 'center',
-			// 'verticalAlign' => 'middle',
-			'y' => 78,
+		'scripts' => [
+			'modules/exporting',
+			'themes/grid-light',
 		],
-		'series' => [
-			[
-				'type' => 'pie',
-				'name' => 'Team E',
-				'innerSize' => '70%',
-				'data' => [
-					[
-						'name' => 'Y',
-						'y' => 10,
-						'color' => '#68BE63', // Jane's color
-					],
-					[
-						'name' => 'N',
-						'y' => 5,
-						'color' => '#95999C', // John's color
-					],
+		'options' => [
+			'title' => [
+				'text' => '30%',
+				// 'align' => 'center',
+				// 'verticalAlign' => 'middle',
+				'y' => 78,
+			],
+			'series' => [
+				[
+					'type' => 'pie',
+					'name' => 'Team E',
+					'innerSize' => '70%',
+					'data' => [
+						[
+							'name' => 'Y',
+							'y' => 10,
+							'color' => '#68BE63', // Jane's color
+						],
+						[
+							'name' => 'N',
+							'y' => 5,
+							'color' => '#95999C', // John's color
+						],
 
+					],
+					'center' => [40, 20],
+					// 'size' => 100,
+					'showInLegend' => false,
+					'dataLabels' => [
+						'enabled' => false,
+					],
 				],
-				'center' => [40, 20],
-				// 'size' => 100,
-				'showInLegend' => false,
-				'dataLabels' => [
+			],
+			'navigation' => [
+				'buttonOptions' => [
 					'enabled' => false,
 				],
 			],
-		],
-		'navigation' => [
-			'buttonOptions' => [
+			'credits' => [
 				'enabled' => false,
 			],
 		],
-		'credits' => [
-			'enabled' => false,
-		],
-	],
-]);
+	]);
 
-?>
+	?>
 				</div>
             </div>
             <div class="col-xs-6 col-sm-3 box-graph">
@@ -401,62 +393,63 @@ echo Highcharts::widget([
                     <div style="overflow: hidden; height: 160px; width: 200;">
                 <?php
 echo Highcharts::widget([
-	'scripts' => [
-		'modules/exporting',
-		'themes/grid-light',
-	],
-	'options' => [
-		'title' => [
-			'text' => '40.2%',
-			// 'align' => 'center',
-			// 'verticalAlign' => 'middle',
-			'y' => 78,
+		'scripts' => [
+			'modules/exporting',
+			'themes/grid-light',
 		],
-		'series' => [
-			[
-				'type' => 'pie',
-				'name' => 'Team F',
-				'innerSize' => '70%',
-				'data' => [
-					[
-						'name' => 'Y',
-						'y' => 10,
-						'color' => '#68BE63', // Jane's color
-					],
-					[
-						'name' => 'N',
-						'y' => 5,
-						'color' => '#95999C', // John's color
-					],
+		'options' => [
+			'title' => [
+				'text' => '40.2%',
+				// 'align' => 'center',
+				// 'verticalAlign' => 'middle',
+				'y' => 78,
+			],
+			'series' => [
+				[
+					'type' => 'pie',
+					'name' => 'Team F',
+					'innerSize' => '70%',
+					'data' => [
+						[
+							'name' => 'Y',
+							'y' => 10,
+							'color' => '#68BE63', // Jane's color
+						],
+						[
+							'name' => 'N',
+							'y' => 5,
+							'color' => '#95999C', // John's color
+						],
 
+					],
+					'center' => [40, 20],
+					// 'size' => 100,
+					'showInLegend' => false,
+					'dataLabels' => [
+						'enabled' => false,
+					],
 				],
-				'center' => [40, 20],
-				// 'size' => 100,
-				'showInLegend' => false,
-				'dataLabels' => [
+			],
+			'navigation' => [
+				'buttonOptions' => [
 					'enabled' => false,
 				],
 			],
-		],
-		'navigation' => [
-			'buttonOptions' => [
+			'credits' => [
 				'enabled' => false,
 			],
 		],
-		'credits' => [
-			'enabled' => false,
-		],
-	],
-]);
+	]);
 
-?>
+	?>
 				</div>
             </div>
 
         </div>
 
-
+<?php }?>
     </div>
     <div class="col-md-4" style="color:#fff;"> List user of team</div>
 
 </div>
+
